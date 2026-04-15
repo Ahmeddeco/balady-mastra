360fe7fa10aefc7e6551fc03a1a31c65ddcece18
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2026-04-15 21:47:15 +0200

    fix: stop tracking .mastra folder and db files

--- .mastra/output/studio/assets/token-util-RMHT2CPJ-CEAUOCJP-Cl7V8ka_.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./chunk-3QLPKWC4-DTsUVKxD.js";import"./main-B3dj5S7e.js";import"./index-DtHsljH9.js";var i=r();export{i as default};
--- .mastra/output/tools.mjs
+++
@@ -1,4 +0,0 @@
-import * as tool0 from './tools/cdb32889-9f67-44f3-a29a-54996b9dad06.mjs';
-import * as tool1 from './tools/80185eaf-2960-4aa2-ad7b-e04d2a3c9757.mjs';
-
-                export const tools = [tool0, tool1]
